fddc5820e86e7fc21dfe685d7b1876b69760dfb0
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-10-04 17:46:46 -0400

    add menu

---
+++ app/javascript/controllers/toggle_controller.js
@@ -0,0 +1,30 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static classes = ["change"];
+  static targets = ["content", "menu"];
+
+  toggle() {
+    this.contentTargets.forEach((elem) => {
+      elem.classList.toggle(this.changeClass);
+    });
+  }
+
+  remove() {
+    this.contentTargets.forEach((elem) => {
+      elem.classList.remove(this.changeClass);
+    });
+  }
+
+  add() {
+    this.contentTargets.forEach((elem) => {
+      elem.classList.add(this.changeClass);
+    });
+  }
+
+  disconnect() {
+    //so menu will not remain open if navigating with the back button
+    const menu = document.getElementById("menu");
+    menu.classList.add("hidden");
+  }
+}
