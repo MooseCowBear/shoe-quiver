999a3233e7fe162a7ef60d7f0180e96aaca08d6a
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-10-16 07:36:24 -0400

    update demo seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,6 +19,9 @@ shoes = [
   { brand: "Asics", model: "MetaSpeed Sky+", color: "neon yellow", category: 2 }
 ]
 
+# 8 shoes, upto 15 runs per shoe... <= 120 dates needed
+recentish_dates = (0..120).to_a 
+
 # curr rotation
 shoes.each do |s|
   shoe = Shoe.new(s)
@@ -26,32 +29,52 @@ shoes.each do |s|
   shoe.save
 
   #add runs
-  num_runs = rand(0..10)
-  num_runs.times do
+  num_runs = rand(0..15)
+  run_dates = recentish_dates.sample(num_runs)
+  recentish_dates.reject! {|elem| run_dates.include?(elem) } #so will get one run per date
+
+  run_dates.each do |d|
     dist = rand(2..26)
     min_dur = dist*6*60
     max_dur = dist*10*60
     dur = rand(min_dur..max_dur)
-    Run.create(shoe: shoe, user: alice, distance: dist, duration: dur, date: Date.today - rand(20), felt: rand(0..3))
+    Run.create(shoe: shoe, user: alice, distance: dist, duration: dur, date: Date.today - d, felt: rand(0..3))
   end
 end
+
+# 24, upto 40 runs per shoe <= 960
+max = 960 - recentish_dates.length + 120
+older_dates = (121..max).to_a
 
 # retired shoes, want enough to test pagination
 3.times do
   shoes.each do |s|
+    num_runs = rand(25..40)
+    run_dates = []
+    if recentish_dates.length >= num_runs
+      run_dates = recentish_dates.sample(num_runs)
+    else
+      run_dates = run_dates + recentish_dates
+      difference = num_runs - run_dates.length
+      other_dates = older_dates.sample(difference)
+      run_dates = run_dates + other_dates
+    end
+
+    recentish_dates.reject! {|elem| run_dates.include?(elem) }
+    older_dates.reject! {|elem| run_dates.include?(elem) }
+    run_dates.sort!
+
     shoe = Shoe.new(s)
     shoe.user = alice
-    last_day = Date.today - rand(200)
-    shoe.retired_on = last_day
+    shoe.retired_on = Date.today - run_dates.last
     shoe.save
 
-    num_runs = rand(30..50)
-    num_runs.times do
+    run_dates.each do |d|
       dist = rand(2..26)
       min_dur = dist*6*60
       max_dur = dist*10*60
       dur = rand(min_dur..max_dur)
-      Run.create(shoe: shoe, user: alice, distance: dist, duration: dur, date: last_day - rand(num_runs), felt: rand(0..3))
+      Run.create(shoe: shoe, user: alice, distance: dist, duration: dur, date: Date.today - d, felt: rand(0..3))
     end
   end
 end
