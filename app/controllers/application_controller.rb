c6509ef0f1a099d0aa3902ea0ce87f5f245d7646
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-10-23 15:03:21 -0400

    remove conversion methods from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,18 +9,6 @@ class ApplicationController < ActionController::Base
   helper_method :km_to_miles
   helper_method :miles_to_km
 
-  # needed bc want to allow user to input mileage as km, but then store it as miles
-  # so this is for conversion of params before storing to the db
-  # def km_to_miles(km)
-  #   (km / 1.60934)
-  # end
-
-  # # needed bc want to allow user to see mileage in kms
-  # # this is for taking db info and displaying it in user's preferred units
-  # def miles_to_km(miles)
-  #   (1.60934 * miles)
-  # end
-
   def confirm_ownership(elem, message)
     if elem.user != current_user
       flash[:alert] = message
