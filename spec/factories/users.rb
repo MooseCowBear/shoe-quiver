eb8b4f9065aa89790e3026da14ca9eb2180be41e
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-01-19 15:00:42 -0500

    update to include devise helpers for system tests, update user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    email { "alice@test.com" }
+    sequence(:email, 100) { |n| "person#{n}@example.com" }
     password { "123456" }
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -68,6 +68,7 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
   config.include Devise::Test::IntegrationHelpers, type: :request
+  config.include Devise::Test::IntegrationHelpers, type: :system
 end
 
 Capybara.server = :puma
